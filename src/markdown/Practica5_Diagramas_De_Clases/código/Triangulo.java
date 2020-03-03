385fca9b14bf1af8578a09fb3526c44223e4cb94
Miguel García Insua <mensajeparamiguel@gmail.com>
2020-03-03 17:37:48 +0100

    cambio ruta

---
+++ src/markdown/Practica5_Diagramas_De_Clases/código/Triangulo.java
@@ -0,0 +1,13 @@
+package miguelClasesPOO;
+
+public class Triangulo extends Figura {
+ 
+	public Triangulo(double base, double altura){
+		super(base, altura);
+	}
+	
+	public double calcularArea(double base, double altura) {
+		double area = (base * altura) / 2;
+		return area;
+	}
+}
